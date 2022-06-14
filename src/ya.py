25a761a0bf1a4ee9bb1a9710981a4007fc4e3dcd
vickychen <vickychen@fillygaming888.com>
2022-06-14 12:12:50 +0800

    Delete test.py

--- src/ya.py
+++
@@ -1,5 +0,0 @@
-from controllers import score_record_routes, school_routes, schedule_routes
-
-
-if __name__ == "__main__":
-    schedule_routes.app.run(debug=True, host='0.0.0.0', port=5000)
--- src/yo.py
+++
@@ -1,8 +0,0 @@
-from test.schedule_test import GETINFO
-# from core.schedule_hanlder import ScheduleHanlder
-
-
-if __name__ in '__main__':
-    GETINFO.update_info()
-    # ScheduleHanlder.get_info()
-
